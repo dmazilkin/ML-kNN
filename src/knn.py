c10387a81eb87471ef83074a3f43d4e51a59688c
Dmitrii Mazilkin <dim.mazilkin@gmail.com>
2025-07-02 12:03:19 +0200

    Add fit method.

--- src/knn.py
+++ src/knn.py
@@ -1,6 +1,15 @@
+import pandas as pd
+
 class MyKNNClf:
     def __init__(self, k: int = 3):
         self.k = k
     
     def __str__(self):
         return f"MyKNNClf class: k={self.k}"
+    
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        self.X = X
+        self.y = y
+        self.train_size = X.shape
+        
+        return self.train_size
