b240733e0eaaa3f2669c0b5104b72dbd633db776
Dmitrii Mazilkin <110918935+dmazilkin@users.noreply.github.com>
2025-07-03 07:29:58 +0200

    Implement KNN class.
    * Initialize KNN class.
    
    * Add fit method.
    
    * Add predict and predict_proba methods, update main with example.

---
+++ main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from typing import Tuple
+
+from src.knn import MyKNNClf
+
+N = 50
+N_test = 10
+
+def create_data() -> Tuple[pd.DataFrame, pd.Series]:
+    np.random.seed(42)
+    X = pd.DataFrame(np.zeros((N, 2)) + 10 * np.random.rand(N, 2))
+    Y = (X.loc[:, 0] > X.loc[:, 1] - 2).astype(int)
+    
+    return X, Y
+
+def create_test_data() -> pd.DataFrame:
+    return pd.DataFrame(np.zeros((N_test, 2)) + 10 * np.random.rand(N_test, 2))
+
+def main():
+    X, y = create_data()
+    model = MyKNNClf(k=10)
+    size = model.fit(X, y)
+    
+    X_test = create_test_data()
+    y_test = model.predict(X_test)
+    y_test_proba = model.predict_proba(X_test)
+    
+    figure, axis = plt.subplots(1, 2)
+    axis[0].scatter(X.to_numpy()[y == 1][:, 0], X.to_numpy()[y == 1][:, 1], color='blue')
+    axis[0].scatter(X.to_numpy()[y == 0][:, 0], X.to_numpy()[y == 0][:, 1], color='red')
+    axis[0].set_title('Real data')
+    
+    axis[1].scatter(X_test.to_numpy()[y_test == 1][:, 0], X_test.to_numpy()[y_test == 1][:, 1], color='blue')
+    axis[1].scatter(X_test.to_numpy()[y_test == 0][:, 0], X_test.to_numpy()[y_test == 0][:, 1], color='red')
+    axis[1].set_title('Predicted data')
+    plt.show()
+    
+if __name__ == '__main__':
+    main()
